584b89127759ddca1e9521de5cd8b4d61eecb64a
Ryan Dumlao <dlogsr@gmail.com>
2013-07-15 22:03:54 -0700

    edits for scrolling

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 $(document).ready(function(){
-	var $navCat = $('.navCat');
-	var $mainBar = $('#mainBar');
-	var $nameBar = $('#nameBar');
-	var $navBar = $('#navBar');
+	var $navCat = $('.navCat'),
+		$mainBar = $('#mainBar'),
+		$nameBar = $('#nameBar'),
+		$navBar = $('#navBar');
 	
-	var $nameBarPrimary = $('#nameBarPrimary');
-	var $nameBarH1 = $('#nameBar h1');
-	var $nameBarH2 = $('#nameBar h2');
+	var $nameBarPrimary = $('#nameBarPrimary'),
+		$nameBarH1 = $('#nameBar h1'),
+		$nameBarH2 = $('#nameBar h2');
 	
-	var $paragraph = $('#paragraph');
+	var $content = $('#content');
+
+	var docked = false;
+	console.log(docked);
 
 	
 	$navCat.click(function() {
-		$mainBar.animate({'bottom':0,
-					      'width':'600px',
-						  'left':0,
-						  'margin-left':0},1000);
-  		$nameBarH1.animate({'font-size':'3em'},1000);
-		$nameBarH2.fadeOut(400);
-		$navBar.animate({'border-top-right-radius':'25px'},100);
-		$navBar.add($nameBar).animate({'height':'50px',
-									   'border-bottom-right-radius':0,
-									   'border-top-left-radius':0,},1000);
-		$nameBarPrimary.animate({'top':'-40px'},1000);
-		$mainBar.removeClass('absolutePos').addClass('fixedPos');
-		$paragraph.fadeIn(1000);
-		$paragraph.removeClass('hidden');
+		if(docked == false){
+			console.log('clicked');
+			$mainBar.animate({'bottom':0,
+						      'width':'600px',
+							  'left':0,
+							  'margin-left':0},1000);
+	  		$nameBarH1.animate({'font-size':'3em'},1000);
+			$nameBarH2.fadeOut(400);
+			$navBar.animate({'border-top-right-radius':'25px'},100);
+			$navBar.add($nameBar).animate({'height':'50px',
+										   'border-bottom-right-radius':0,
+										   'border-top-left-radius':0,},1000);
+			$nameBarPrimary.animate({'top':'-40px'},1000);
+			$mainBar.removeClass('absolutePos').addClass('fixedPos');
+			$content.fadeIn(1000);
+			$content.removeClass('hidden');
+			docked = true;
+		}
+		else{
+		};
+		console.log(this.attr('id'));
+		var offset = $navCat.offset().top - 20;
+		$('html,body').stop().animate({scrollTop : offset},1000);
+
 
 	});
 	
